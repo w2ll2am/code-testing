b6392fd2d3406072bf8d3511b8f5eb1f0b20c684
w2ll2am <w.boothclibborn@gmail.com>
2021-06-29 09:31:19 +0100

    Update HPC-job-submission.sh

--- HPC-job-submission.sh
+++ HPC-job-submission.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=00:01:00 # maximum walltime for the job
 #SBATCH -A Research_Project-T119422 # research project to submit under
 #SBATCH --nodes=1 # specify number of nodes
-#SBATCH --ntasks-per-node=8 # specify number of processors per node
+#SBATCH --ntasks-per-node=2 # specify number of processors per node
 #SBATCH --mem=1G # specify bytes memory to reserve
 #SBATCH --mail-type=END # send email at job completion
 #SBATCH --mail-user=arb233@exeter.ac.uk # email address
