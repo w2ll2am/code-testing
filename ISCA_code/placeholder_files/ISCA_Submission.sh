069873d04ad31723fa7ca7040b4c9659fdeedb29
William Booth-Clibborn <boysboothclibborn@gmail.com>
2022-08-07 23:48:49 +0100

    Add isca code

---
+++ ISCA_code/placeholder_files/ISCA_Submission.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --export=ALL # export all environment variables to the batch job
+#SBATCH -D . # set working directory to .
+#SBATCH -p pq # submit to the parallel queue
+#SBATCH --time=6:00:00 # maximum walltime for the job
+#SBATCH -A Reseach_Project-CEMPS-00006 # research project to submit under
+#SBATCH --nodes=1 # specify number of nodes
+#SBATCH --ntasks-per-node=$1 # specify number of processors per node
+#SBATCH --mem=40GB # specify bytes memory to reserve
+#SBATCH --mail-type=END # send email at job completion
+#SBATCH --mail-user=wb342@exeter.ac.uk # email address
+#Commands you wish to run must go here, after the SLURM directives
+
+mpirun -np $1 python "Hight_Normalisation_(KHH).py"
+python merge.py snapshots
+python3 plot.py
+ffmpeg -framerate 25 -i plot%04d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p Animation.mp4
